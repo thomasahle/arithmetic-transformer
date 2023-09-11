592936cddfd3939f70ed6dbabbe9033bc350503b
Thomas D Ahle <thomas@ahle.dk>
2023-09-11 14:02:31 -0700

    Simplified code

--- model.py
+++ model.py
@@ -1,22 +1,23 @@
-
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, random_split
 
 from dataset import AdditionDataset
-import methods
-
-def causal_mask(x):
-    """ matrix with -inf above the diagonal and 0 on the diagonal and below """
-    sz = len(x)
-    mask = torch.ones(sz, sz).triu(1)
-    mask = mask.masked_fill(mask == 1, float('-inf'))
-    return mask.to(x.device)
 
 
-class AdditionModel(pl.LightningModule):
-    def __init__(self, kind, ds, hidden_size, num_layers, num_heads, num_queries, lr, norm_first, grouped, is_cosine, norm_kvs, dropout, noise_rank):
+class AdditionModel(nn.Module):
+    def __init__(
+        self,
+        kind,
+        ds,
+        hidden_size,
+        num_layers,
+        num_heads,
+        lr,
+        norm_first,
+        dropout,
+    ):
         super().__init__()
         self.ds = ds  # Input the dataset for relevant parameters
         self.lr = lr
@@ -26,44 +27,33 @@ class AdditionModel(pl.LightningModule):
         self.kind = kind
         seq = self.ds.seq
         if kind == "lstm":
-            self.model = nn.LSTM( input_size=hidden_size, hidden_size=hidden_size, num_layers=num_layers, dropout=dropout)
+            self.model = nn.LSTM(
+                input_size=hidden_size,
+                hidden_size=hidden_size,
+                num_layers=num_layers,
+                dropout=dropout,
+            )
         elif kind == "rnn":
-            self.model = nn.RNN( input_size=hidden_size, hidden_size=hidden_size, num_layers=num_layers, dropout=dropout)
+            self.model = nn.RNN(
+                input_size=hidden_size,
+                hidden_size=hidden_size,
+                num_layers=num_layers,
+                dropout=dropout,
+            )
         elif kind == "gru":
-            self.model = nn.GRU( input_size=hidden_size, hidden_size=hidden_size, num_layers=num_layers, dropout=dropout)
-        elif kind == 'myformer':
-            self.pos_emb = nn.Embedding(seq, hidden_size)
-            self.model = methods.MyTransformer(
-                n_layers=num_layers,
-                dim=hidden_size,
-                n_queries=num_queries,
-                n_keys=num_heads,
-                norm_first=norm_first,
-                grouped=grouped,
-                is_cosine=is_cosine,
-                norm_kvs=norm_kvs,
+            self.model = nn.GRU(
+                input_size=hidden_size,
+                hidden_size=hidden_size,
+                num_layers=num_layers,
                 dropout=dropout,
-                noise_rank=noise_rank,
             )
-        elif kind == 'revformer':
-            self.pos_emb = nn.Embedding(seq, hidden_size)
-            self.model = methods.RevTransformer(
-                n_layers=num_layers,
-                dim=hidden_size,
-                n_queries=num_queries,
-                n_keys=num_heads,
-                norm_first=norm_first,
-                grouped=grouped,
-                is_cosine=is_cosine,
-                norm_kvs=norm_kvs,
-                dropout=dropout,
-                noise_rank=noise_rank,
-            )
-        elif kind == "transformer":
+        elif kind in ("transformer", "transformer-nope"):
             self.pos_emb = nn.Embedding(seq, hidden_size)
             self.model = nn.TransformerEncoder(
                 nn.TransformerEncoderLayer(
-                    d_model=hidden_size, dim_feedforward=hidden_size * 2, nhead=num_heads,
+                    d_model=hidden_size,
+                    dim_feedforward=hidden_size * 2,
+                    nhead=num_heads,
                     norm_first=norm_first,
                     dropout=dropout,
                 ),
@@ -80,26 +70,14 @@ class AdditionModel(pl.LightningModule):
             )
             self.model2 = nn.TransformerEncoder(
                 nn.TransformerEncoderLayer(
-                    d_model=hidden_size, dim_feedforward=hidden_size * 2, nhead=num_heads,
+                    d_model=hidden_size,
+                    dim_feedforward=hidden_size * 2,
+                    nhead=num_heads,
                     norm_first=norm_first,
                     dropout=dropout,
                 ),
                 num_layers // 2,
             )
-        elif kind == "mlp":
-            # Note, the whole CausalMLP approach doesn't work when the sequence length can increase
-            if num_layers == 1:
-                print("Warning, MLP doesn't support single layer")
-            layers = [methods.TriangularLinear(hidden_size * seq), nn.ReLU()]
-            for _ in range(num_layers - 2):
-                layers.append(methods.TriangularLinear(hidden_size * seq))
-                layers.append(nn.ReLU())
-            layers.append(methods.TriangularLinear(hidden_size * seq))
-            self.model = nn.Sequential(*layers)
-        elif kind == "mlp2":
-            self.model = methods.CausalMLP(num_layers, seq, hidden_size, rank=num_heads)
-        elif kind == "res":
-            self.model = methods.ResNet(n_layers=num_layers, dim=hidden_size, groups=num_heads)
         else:
             raise Error(f"Kind {kind} is not supported")
 
@@ -110,170 +88,74 @@ class AdditionModel(pl.LightningModule):
         # Note lstm assumes (seq, batch, feature)
         x = emb.permute(1, 0, 2)
 
-        if hasattr(self, 'pos_emb') and self.pos_emb.num_embeddings < x.size(0):
-            print(f"Increasing pos embedding size from {self.pos_emb.num_embeddings} to {x.size(0)}")
+        if hasattr(self, "pos_emb") and self.pos_emb.num_embeddings < x.size(0):
+            print(
+                f"Increasing pos embedding size from {self.pos_emb.num_embeddings} to {x.size(0)}"
+            )
             with torch.no_grad():
-                new_pos_emb = nn.Embedding(x.size(0), self.pos_emb.embedding_dim).to(x.device)
-                new_pos_emb.weight[:self.pos_emb.num_embeddings] = self.pos_emb.weight
+                new_pos_emb = nn.Embedding(x.size(0), self.pos_emb.embedding_dim).to(
+                    x.device
+                )
+                new_pos_emb.weight[: self.pos_emb.num_embeddings] = self.pos_emb.weight
                 self.pos_emb = new_pos_emb
 
         if self.kind in ("lstm", "rnn", "gru"):
             x, _ = self.model(x)
-        elif self.kind in ("myformer", "revformer"):
+        elif self.kind in ("transformer", "transformer-nope"):
             positions = torch.arange(0, x.size(0)).unsqueeze(0).to(x.device)
-            emb = self.pos_emb(positions).permute(1, 0, 2).to(x.device)
-            x = self.model(x + emb)
-        elif self.kind == "transformer":
-            positions = torch.arange(0, x.size(0)).unsqueeze(0).to(x.device)
-            emb = self.pos_emb(positions).permute(1, 0, 2).to(x.device)
-            #x = self.model(x + emb, torch.zeros_like(x), tgt_mask=causal_mask(x))
-            attn_mask = nn.Transformer.generate_square_subsequent_mask(x.shape[1], x.device)
-            x = self.model(x + emb, mask=attn_mask, is_causal=True)
-            # Add an extra linear layer, just becuase I'm doing this in myformer.
+            if self.kind != "transformer-nope":
+                emb = self.pos_emb(positions).permute(1, 0, 2).to(x.device)
+                x = x + emb
+            attn_mask = nn.Transformer.generate_square_subsequent_mask(
+                x.shape[1], x.device
+            )
+            x = self.model(x, mask=attn_mask, is_causal=True)
             x = self.fc(self.norm(x))
         elif self.kind == "hybrid":
             x, _ = self.model1(x)
-            #x = self.model2(x, torch.zeros_like(x), tgt_mask=causal_mask(x))
-            attn_mask = nn.Transformer.generate_square_subsequent_mask(x.shape[1], x.device)
+            attn_mask = nn.Transformer.generate_square_subsequent_mask(
+                x.shape[1], x.device
+            )
             x = self.model2(x, attn_mask, is_causal=True)
-        elif self.kind == "mlp":
-            x = self.model(emb.flatten(start_dim=1)).reshape(emb.shape)
-            x = x.permute(1, 0, 2)
-        elif self.kind in ("mlp2", "res"):
-            x = self.model(x)
         # Re-permute to (batch, seq, dim)
         x = x.permute(1, 0, 2)
         # Might as well reuse the embeddings
         return x @ self.embedding.weight.T
-
-
-    def training_step(self, batch, batch_idx):
-        # Mask of everything from end_token and to the right
-        mask = torch.cumsum(batch == self.ds.end_token, dim=1)[:, 1:]
-        truth = batch[:, 1:][mask == 1]
-        out = self(batch)[:, :-1][mask == 1]
-
-        # Compute loss only on the targets, not the input
-        loss = nn.functional.cross_entropy(out, truth)
-
-        # Bayesian loss
-        bayes_loss = 0
-        for module in self.modules():
-            if hasattr(module, 'penalty'):
-                bayes_loss += module.penalty()
-        # 1e-5 gives a very janky learning. 1e-3 is often too much and doesn't
-        # allow the model to learn.
-        loss += 1e-4 * bayes_loss
-
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        # We validate only past the end-token
-        mask = torch.cumsum(batch == self.ds.end_token, dim=1)[:, 1:]
-        # We shift everything by one, since we are autoregressive
-        truth = batch[:, 1:][mask == 1]
-        out = self(batch)[:, :-1]
-        logits = out[mask == 1]
-
-        loss = nn.functional.cross_entropy(logits, truth)
-        self.log("val_loss", loss, prog_bar=True)
-
-        wn = 0
-        for pn, p in self.named_parameters():
-            if p.requires_grad and p.dim() >= 2:
-                wn += (p ** 2).mean()
-        self.log("weigth_norm", wn, prog_bar=True)
-
-        # bl = 0
-        # for module in self.modules():
-        #     if hasattr(module, 'penalty'):
-        #         bl += module.penalty()
-        # self.log("bayes_loss", bl, prog_bar=True)
-
-        # Count only test cases completely solved.
-        # Otherwise we get too many points for predicting padding.
-        truth2 = batch[:, 1:] * mask
-        preds = torch.argmax(out, dim=2) * mask
-        acc = torch.all(preds == truth2, dim=1).float().mean()
-        self.log("val_acc", acc, prog_bar=True)
-
-        # if self.kind == 'mlp2' and batch_idx == 0 and hasattr(self.logger.experiment, 'log'):
-        #     w2sum = 0
-        #     for i, block in enumerate(self.model.blocks):
-        #         for j, causal in enumerate(block.causals):
-        #             w2 = causal.weight**2
-        #             w2sum += w2
-        #             wandb_img = wandb.Image(cm.hot(w2.cpu().numpy()), caption="Causal Norms")
-        #             self.logger.experiment.log({f"causal norms {i} head {j}": wandb_img}, step=self.global_step)
-
-        #     w2sum /= len(self.model.blocks)
-        #     wandb_img = wandb.Image(cm.hot(w2sum.cpu().numpy()), caption="Causal Norms")
-        #     self.logger.experiment.log({"causal norms": wandb_img}, step=self.global_step)
-
-        return acc
 
     def configure_optimizers(self):
         param_dict = {pn: p for pn, p in self.named_parameters() if p.requires_grad}
         decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
         nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
         optim_groups = [
-            {'params': decay_params, 'weight_decay': 1e-2},
-            {'params': nodecay_params, 'weight_decay': 0.0}
+            {"params": decay_params, "weight_decay": 1e-2},
+            {"params": nodecay_params, "weight_decay": 0.0},
         ]
         num_decay_params = sum(p.numel() for p in decay_params)
         num_nodecay_params = sum(p.numel() for p in nodecay_params)
-        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
-        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        print(
+            f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters"
+        )
+        print(
+            f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters"
+        )
         # Create AdamW optimizer and use the fused version if it is available
-        optimizer = torch.optim.AdamW(optim_groups, lr=self.lr, fused=torch.cuda.is_available())
+        optimizer = torch.optim.AdamW(
+            optim_groups, lr=self.lr, fused=torch.cuda.is_available()
+        )
         # optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
-
-    # def train_dataloader(self):
-    #     # Return a DataLoader for the newly created dataset.
-    #     # The idea is to make it bigger as we learn
-    #     return DataLoader(self.ds, batch_size=self.batch_size, num_workers=1)
-
-    # def val_dataloader(self):
-    #     # Make a smaller dataset for validation
-    #     print(f"Making validation dataloader with number_length={self.ds.number_length}")
-    #     dataset = AdditionDataset(
-    #         self.ds.num_samples // 10,
-    #         base=self.ds.base,
-    #         number_length=self.ds.number_length,
-    #         min_sequence_length=self.ds.sequence_length,
-    #         sequence_length=self.ds.sequence_length,
-    #     )
-    #     return DataLoader(dataset, batch_size=self.batch_size, num_workers=1)
-
-    def on_validation_epoch_end(self):
-        # Get the logged accuracy for the current epoch
-        current_val_acc = self.trainer.callback_metrics.get('val_acc', 0)  # Assuming 'val_acc' is the key you use to log accuracy
-
-        print(f"\nGot validation accuracy {current_val_acc}.")
-        self.print_examples()
-
-        if current_val_acc > 0.9:
-            # Switch to a more difficult dataset
-            print(f"Switching to number length {self.ds.number_length + 1}!")
-            self.ds = AdditionDataset(
-                self.ds.num_samples,
-                base=self.ds.base,
-                # Increase number length
-                number_length=self.ds.number_length + 1,
-                min_sequence_length=self.ds.sequence_length,
-                sequence_length=self.ds.sequence_length,
-            )
 
     def generate(self, input_sequence):
         # Generate output sequence from input sequence using trained model
         assert input_sequence[-1] == self.ds.end_token
         # Pad to expected length
         n = len(input_sequence)
-        input_sequence = torch.cat([
-            torch.tensor(input_sequence),
-            torch.tensor([self.ds.padding_token] * (self.ds.seq - n), dtype=int)
-        ]).to(self.device)
+        input_sequence = torch.cat(
+            [
+                torch.tensor(input_sequence),
+                torch.tensor([self.ds.padding_token] * (self.ds.seq - n), dtype=int),
+            ]
+        ).to(self.embedding.weight.device)
         with torch.no_grad():
             for i in range(n, self.ds.seq):
                 output_logits = self(input_sequence[None])[0, i - 1]
@@ -283,21 +165,19 @@ class AdditionModel(pl.LightningModule):
 
     def print_examples(self, num_examples=3):
         with torch.no_grad():
-            dic = {i: str(i) for i in range(self.ds.base+1)}
-            dic[self.ds.padding_token] = ''
-            dic[self.ds.end_token] = ','
-            dic[self.ds.eos_token] = ''
-            dic[self.ds.separator_token] = ','
+            dic = {i: str(i) for i in range(self.ds.base + 1)}
+            dic[self.ds.padding_token] = ""
+            dic[self.ds.end_token] = ","
+            dic[self.ds.eos_token] = ""
+            dic[self.ds.separator_token] = ","
             for example in self.ds.generate_batch(num_examples):
                 example = list(example.cpu().numpy())
-                print('Data:   ', example)
-                # [list(group) for k, group in itertools.groupby(l, lambda x: x >= 2) if not k]
-                string = ''.join(dic[t] for t in example)
-                nums = [num for num in string.split(',')]
-                print("Example:", ' + '.join(nums[:-1]), '=', nums[-1])
+                print("Data:   ", example)
+                string = "".join(dic[t] for t in example)
+                nums = [num for num in string.split(",")]
+                print("Example:", " + ".join(nums[:-1]), "=", nums[-1])
 
                 n = example.index(self.ds.end_token) + 1
                 prediction = self.generate(example[:n]).cpu().numpy()
-                string = ''.join(dic[t] for t in prediction)
-                print("Output: ", string, '- Raw:', prediction)
-
+                string = "".join(dic[t] for t in prediction)
+                print("Output: ", string, "- Raw:", prediction)
--- train.py
+++ train.py
@@ -1,20 +1,12 @@
 import argparse
 import itertools
 import torch
-from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
-from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.tuner import Tuner
-import wandb
-from collections import OrderedDict
-import inspect
-import pytorch_lightning as pl
 import tqdm
 from collections import Counter
 import torch.nn.functional as F
 
 from dataset import AdditionDataset
 from model import AdditionModel
-
 
 
 def main():
@@ -31,9 +23,10 @@ def main():
         help="Number of examples to generate and train on",
     )
     parser.add_argument("--lr", type=float, default=1e-3, help="Adam LR")
-    parser.add_argument("--acc-next", type=float, default=.9, help="Accuracy before next level")
+    parser.add_argument(
+        "--acc-next", type=float, default=0.9, help="Accuracy before next level"
+    )
     parser.add_argument("--dropout", type=float, default=0)
-    parser.add_argument("--noise-rank", type=int, default=10)
     parser.add_argument(
         "--hidden-size",
         type=int,
@@ -57,26 +50,20 @@ def main():
         help="The type of neural network to use (lstm, transformer, hybrid)",
     )
     parser.add_argument("--tune-lr", action="store_true")
-    parser.add_argument("--wandb", action="store_true")
     parser.add_argument("--norm-last", action="store_true")
-    parser.add_argument("--cosine", action="store_true")
-    parser.add_argument("--grouped", action="store_true")
-    parser.add_argument("--norm-kvs", action="store_true")
     parser.add_argument("--cpu", action="store_true")
-    parser.add_argument("--num-heads", type=int, default=1, help="The number of heads/rank in transformer/mlp")
-    parser.add_argument("--num-queries", type=int, default=1)
     parser.add_argument(
-        "--tag",
-        type=str,
-        default="",
+        "--num-heads",
+        type=int,
+        default=1,
+        help="The number of heads/rank in transformer/mlp",
     )
     args = parser.parse_args()
 
     dataset = AdditionDataset(
-        10**6, # data points per epoch
+        10**6,  # data points per epoch
         base=10,
         number_length=1,
-        sequence_length=2,
     )
 
     model = AdditionModel(
@@ -85,46 +72,27 @@ def main():
         hidden_size=args.hidden_size,
         num_layers=args.num_layers,
         num_heads=args.num_heads,
-        num_queries=args.num_queries,
         norm_first=not args.norm_last,
-        is_cosine=args.cosine,
-        grouped=args.grouped,
-        norm_kvs=args.norm_kvs,
         lr=args.lr,
         dropout=args.dropout,
-        noise_rank=args.noise_rank,
     )
     num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f'The model has {num_params} parameters')
+    print(f"The model has {num_params} parameters")
 
     # model = torch.compile(model)
     manual_training(model, dataset, args)
 
-def lightning_training(model, dataset, args):
-    if args.wandb:
-        logger = WandbLogger(
-            name=f"{args.kind}_p={format_size(num_params)}_base={args.base}_seqlen={args.sequence_length}_layers={args.num_layers}_hidden={args.hidden_size}_heads={args.num_heads}_qs={args.num_queries}_lr={args.lr}_norm_first={not args.norm_last}_grouped={args.grouped}_cosine={args.cosine}_normkvs={args.norm_kvs}_dropout={args.dropout}_noise_rank={args.noise_rank}_tag={args.tag}",
-            log_model=True,
-        )
-        wandb.init('add lstm')
-        wandb.watch(model, log="all")
-        trainer = pl.Trainer(max_epochs=args.epochs, logger=logger, reload_dataloaders_every_n_epochs=1)
-    else:
-        trainer = pl.Trainer(max_epochs=args.epochs, reload_dataloaders_every_n_epochs=1)
-
-    trainer.fit(model)
-
 
 def manual_training(model, dataset, args):
     if args.cpu:
-        device = torch.device('cpu')
+        device = torch.device("cpu")
     else:
         if torch.cuda.is_available():
-            device = torch.device('cuda')
+            device = torch.device("cuda")
         elif torch.backends.mps.is_available():
-            device = torch.device('mps')
+            device = torch.device("mps")
         else:
-            device = torch.device('cpu')
+            device = torch.device("cpu")
     model = model.to(device)
 
     # Get optimizer (and potentially the scheduler)
@@ -139,15 +107,19 @@ def manual_training(model, dataset, args):
     for epoch in range(args.epochs):
         train_batches = 1000
         with torch.no_grad():
-            X = dataset.generate_batch(args.batch_size * train_batches).to(model.device)
-            Xmask = (torch.cumsum(X == dataset.end_token, dim=1) == 1) & (X != dataset.end_token)
+            X = dataset.generate_batch(args.batch_size * train_batches).to(device)
+            Xmask = (torch.cumsum(X == dataset.end_token, dim=1) == 1) & (
+                X != dataset.end_token
+            )
             Xmask = Xmask[:, 1:]
 
         # Training Loop
         model.train()
         for batch_idx in tqdm.tqdm(range(train_batches)):
-            batch = X[batch_idx*args.batch_size : (batch_idx+1)*args.batch_size]
-            mask = Xmask[batch_idx*args.batch_size : (batch_idx+1)*args.batch_size]
+            batch = X[batch_idx * args.batch_size : (batch_idx + 1) * args.batch_size]
+            mask = Xmask[
+                batch_idx * args.batch_size : (batch_idx + 1) * args.batch_size
+            ]
 
             optimizer.zero_grad()
             truth = batch[:, 1:]
@@ -163,13 +135,19 @@ def manual_training(model, dataset, args):
         model.eval()
         with torch.no_grad():
             val_batches = 100
-            X = dataset.generate_batch(args.batch_size * val_batches).to(model.device)
-            Xmask = (torch.cumsum(X == dataset.end_token, dim=1) == 1) & (X != dataset.end_token)
+            X = dataset.generate_batch(args.batch_size * val_batches).to(device)
+            Xmask = (torch.cumsum(X == dataset.end_token, dim=1) == 1) & (
+                X != dataset.end_token
+            )
             Xmask = Xmask[:, 1:]
 
             for batch_idx in tqdm.tqdm(range(val_batches)):
-                batch = X[batch_idx*args.batch_size : (batch_idx+1)*args.batch_size]
-                mask = Xmask[batch_idx*args.batch_size : (batch_idx+1)*args.batch_size]
+                batch = X[
+                    batch_idx * args.batch_size : (batch_idx + 1) * args.batch_size
+                ]
+                mask = Xmask[
+                    batch_idx * args.batch_size : (batch_idx + 1) * args.batch_size
+                ]
 
                 truth2 = batch[:, 1:] * mask
                 out = model(batch)[:, :-1]
