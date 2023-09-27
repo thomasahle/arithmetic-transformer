23aca5316ed610064c18ffd67999eaa573d13a7a
Thomas D Ahle <thomas@ahle.dk>
2023-09-26 21:02:40 -0400

    Runner script for testing different arguments

--- runner.py
+++ runner.py
@@ -2,6 +2,8 @@ import subprocess
 import re
 import sys
 from tqdm import tqdm
+from tabulate import tabulate
+
 
 def run_program_with_parameters(num_layers, num_heads):
     # Run the program with the given parameters
@@ -12,9 +14,9 @@ def run_program_with_parameters(num_layers, num_heads):
         "--val-batches", "10",
         "--batch-size", "1000",
         "--kind", "transformer-nope",
-        "--epochs", "3",
+        "--epochs", "2",
         "--flip",
-        "--hidden-size", "30",
+        "--hidden-size", "60",
         "--num-layers", str(num_layers),
         "--num-heads", str(num_heads)
     ]
@@ -30,15 +32,28 @@ def run_program_with_parameters(num_layers, num_heads):
     else:
         return None
 
+
+def print_table(headers, rows):
+    # Clear previous table display
+    for _ in range(len(rows) + 2):  # +2 accounts for headers and initial table display line
+        sys.stdout.write("\033[F")  # Move cursor up one line
+        sys.stdout.write("\033[K")  # Clear current line
+    # Print updated table
+    print(tabulate(rows, headers=headers, tablefmt='grid'))
+
+
 def main():
-    num_layers_values = [1, 2, 3, 4]  # example values, adjust as needed
-    num_heads_values = [1, 2, 3, 4]  # example values, adjust as needed
+    num_layers_values = list(range(1, 7))  # example values, adjust as needed
+    num_heads_values = list(range(1, 7))  # example values, adjust as needed
     
     best_value = 0
     best_parameters = None
-    
+
     # 2D list to store results
-    results_table = [[0 for _ in num_heads_values] for _ in num_layers_values]
+    results_table = [["-" for _ in num_heads_values] for _ in num_layers_values]
+    
+    headers = ["num-layers/num-heads"] + num_heads_values
+    print(tabulate([["-" for _ in headers]], headers=headers, tablefmt='grid'))  # Initial table display
 
     with tqdm(total=len(num_layers_values) * len(num_heads_values), desc="Progress") as pbar:
         for i, num_layers in enumerate(num_layers_values):
@@ -49,15 +64,15 @@ def main():
                     if result > best_value:
                         best_value = result
                         best_parameters = (num_layers, num_heads)
-                pbar.update(1)
                 
-    # Print the results in a table
-    print("\nResults Table:")
-    headers = ["num-layers/num-heads"] + num_heads_values
-    rows = [[num_layers] + row for num_layers, row in zip(num_layers_values, results_table)]
-    print(tabulate(rows, headers=headers, tablefmt='grid'))
+                # Print the table with the updated result
+                rows = [[num_layers] + row for num_layers, row in zip(num_layers_values, results_table)]
+                print_table(headers, rows)
+                
+                pbar.update(1)
 
     print(f"\nThe best value is {best_value} with num-layers = {best_parameters[0]} and num-heads = {best_parameters[1]}")
 
 if __name__ == "__main__":
     main()
+
